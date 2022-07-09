7191efef0e7c2f1b81e7034edc3a5a3b0c301824
just9krish <just9krish434@gamil.com>
2022-07-09 12:07:18 +0530

    completed with js for switching content on click

--- main.js
+++ main.js
@@ -5,34 +5,69 @@ let weeklyPrevious = [];
 let monthlyCurrent = [];
 let monthlyPrevious = [];
 
-
 // Getting data from json
 fetch("./data.json")
-.then(response => response.json())
-.then(jsonData => {
+  .then((response) => response.json())
+  .then((jsonData) => {
     jsonData.forEach((e) => {
-        dailyCurrent.push(e.timeframes.daily.current)
-        dailyPrevious.push(e.timeframes.daily.previous)
-        weeklyCurrent.push(e.timeframes.weekly.current)
-        weeklyPrevious.push(e.timeframes.weekly.previous)
-        monthlyCurrent.push(e.timeframes.monthly.current)
-        monthlyPrevious.push(e.timeframes.monthly.previous)
-    })
-})
+      dailyCurrent.push(e.timeframes.daily.current);
+      dailyPrevious.push(e.timeframes.daily.previous);
+      weeklyCurrent.push(e.timeframes.weekly.current);
+      weeklyPrevious.push(e.timeframes.weekly.previous);
+      monthlyCurrent.push(e.timeframes.monthly.current);
+      monthlyPrevious.push(e.timeframes.monthly.previous);
+    });
+  })
 
-.then(() => {
-    const current = document.getElementsByClassName("hours")
-    const previous = document.getElementsByClassName("previous-hours")
-    
+  .then(() => {
+    const current = document.getElementsByClassName("hours");
+    const previous = document.getElementsByClassName("previous-hours");
+
     for (let i = 0; i < 6; i++) {
-        current[i].innerHTML = `${dailyCurrent[i]} hrs`;
-        previous[i].innerHTML = `Yesterday - ${dailyPrevious[i]} hrs`;
+      current[i].innerHTML = `${dailyCurrent[i]} hrs`;
+      previous[i].innerHTML = `Yesterday - ${dailyPrevious[i]} hrs`;
     }
-})
+  });
 
-// console.log(dailyCurrent)
-// console.log(dailyPrevious)
-// console.log(weeklyCurrent)
-// console.log(weeklyPrevious)
-// console.log(monthlyCurrent)
-// console.log(monthlyPrevious)
+changeDuration();
+
+function changeDuration(duration) {
+  let target = document.getElementsByName("duration");
+
+  target.forEach((target) => {
+    target.addEventListener("click", () => {
+      if (target.checked) {
+        let duration = target.value;
+
+        const hours = document.getElementsByClassName("hours");
+        const previousHours = document.getElementsByClassName("previous-hours");
+        let current;
+        let previous;
+        let range;
+
+        switch (duration) {
+          case "daily":
+            current = dailyCurrent;
+            previous = dailyPrevious;
+            range = "Yesterday";
+            break;
+          case "weekly":
+            current = weeklyCurrent;
+            range = "Last Week";
+            previous = weeklyPrevious;
+            break;
+          case "monthly":
+            current = monthlyCurrent;
+            range = "Last Month";
+            previous = monthlyPrevious;
+            break;
+        }
+
+        for (let i = 0; i < 6; i++) {
+          hours[i].innerHTML = `${current[i]} hrs`;
+          previousHours[i].innerHTML = `${range} - ${previous[i]} hrs`;
+        }
+      }
+    });
+  });
+}
